6cb1d246d834dc440927b64b8147a1def5580f30
Pascal Eckmann <p4cx@protonmail.com>
2020-01-03 17:31:04 +0100

    fix startup on raspberry pi

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd /home/pi/
-python3 ./optaradio/web/main.py &
+cd /home/pi/optaradio/
+python3 ./web/main.py &
 export DISPLAY=:0
 X -nocursor &
-python3 ./optaradio/optaradio/init.py &
+python3 ./optaradio/init.py &
 
