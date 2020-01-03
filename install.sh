a811425e6f18f9e1a8962844f626445385c0ad65
Pascal Eckmann <p4cx@protonmail.com>
2020-01-03 16:48:49 +0100

    install libsdl1.2 for pygame on raspberry

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 cd /home/pi
 sudo apt-get update
-sudo apt-get install imagemagick vlc python3 python3-pip -y
+sudo apt-get install imagemagick vlc python3 python3-pip libsdl1.2-dev -y
 sudo pip3 install -r ./optaradio/requirements.txt
