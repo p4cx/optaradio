a1744d02c658f2daaa00b1d671e9e65c9e75324b
Pascal Eckmann <p4cx@protonmail.com>
2020-01-03 16:44:52 +0100

    install should use pip3 instead of pip

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 cd /home/pi
 sudo apt-get update
 sudo apt-get install imagemagick vlc python3 python3-pip -y
-sudo pip install -r ./optaradio/requirements.txt
+sudo pip3 install -r ./optaradio/requirements.txt
