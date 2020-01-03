bbd08511aeb3ece58d625ff002b75ff90733c22f
Pascal Eckmann <p4cx@protonmail.com>
2020-01-03 16:58:12 +0100

    possible fix for pygame on raspberry pi

--- install.sh
+++ install.sh
@@ -2,5 +2,9 @@
 
 cd /home/pi
 sudo apt-get update
-sudo apt-get install imagemagick vlc python3 python3-pip libsdl1.2-dev -y
+sudo cp /etc/apt/sources.list /etc/apt/sources.list~
+sudo sed -Ei 's/^# deb-src /deb-src /' /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get install imagemagick vlc python3 python3-pip -y
+sudo apt-get build-dep python-pygame
 sudo pip3 install -r ./optaradio/requirements.txt
