a034a0cb3554969aa39aa6a8fcbde62e9617bda8
Pascal Eckmann <p4cx@protonmail.com>
2020-01-04 19:43:20 +0100

    add correct path to header images

--- web/globals.py
+++ web/globals.py
@@ -3,7 +3,7 @@ import os
 RES_PATH = os.getcwd() + '/res/'
 STATIC_PATH = os.getcwd() + '/static/'
 TEMP_PATH = os.getcwd() + '/temp/'
-THUMBS_PATH = RES_PATH + 'radio_thumbs/'
+THUMBS_PATH = os.path.abspath(os.path.realpath(os.path.join(os.getcwd(), '../optaradio/res/radio_thumbs/')))
 APP_ICON = RES_PATH + 'icons/app_icon.png'
 RADIO_STATION_CSV_PATH = os.path.abspath(os.path.realpath(os.path.join(os.getcwd(), '../optaradio/res/radio.csv')))
 UNICODE_FLAG_FILE = STATIC_PATH + 'emoji-test.txt'
