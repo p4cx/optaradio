18a3b19d99b670099a756568575fc8f8ecf378aa
Pascal Eckmann <p4cx@protonmail.com>
2020-01-04 11:15:45 +0100

    uwsgi doesnt recognize relative paths here

--- web/globals.py
+++ web/globals.py
@@ -5,6 +5,6 @@ STATIC_PATH = os.getcwd() + '/static/'
 TEMP_PATH = os.getcwd() + '/temp/'
 THUMBS_PATH = RES_PATH + 'radio_thumbs/'
 APP_ICON = RES_PATH + 'icons/app_icon.png'
-RADIO_STATION_CSV_PATH = os.path.join(os.getcwd(), '../optaradio/res/radio.csv')
+RADIO_STATION_CSV_PATH = os.path.abspath(os.path.realpath(os.path.join(os.getcwd(), '../optaradio/res/radio.csv')))
 UNICODE_FLAG_FILE = STATIC_PATH + 'emoji-test.txt'
 UNICODE_COUNTRIES_LIST = STATIC_PATH + 'countries.csv'
